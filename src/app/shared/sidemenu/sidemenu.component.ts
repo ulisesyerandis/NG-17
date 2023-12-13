09f3f4d9a025ffc53b7d006a10bd4f1143caf1bd
unknown <uyerandisr@gmail.com>
2023-12-12 19:32:45 -0500

    finich the routes in dashboard

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css'
 })
-export class SidemenuComponent {
+export class SidemenuComponent 
+{
+    // configuration to the sidemenu,
+    //  to (barrer todas las rutas) navigate or acces to all routes from the sidemenu
+    //                          first way   FROM A VARIABLE
+    public menuItems = routes
+    .map( routes => routes.children ?? [])
+    .flat()
+    .filter(route => route && route.path )
+    .filter(routes => routes && !routes.path?.includes(':'))
 
+  constructor ()
+  { 
+    // configuration to the sidemenu,
+    //  to (barrer todas las rutas) navigate or acces to all routes from the sidemenu
+    //                    secund way    FROM  THE CONSTRUCTOR
+    // const dashboardRoutes = routes
+    // .map( routes => routes.children ?? [])
+    // .flat()
+    // .filter(route => route && route.path )
+    // .filter(routes => routes && !routes.path?.includes(':'))
+
+    // console.log(dashboardRoutes);
+  }
 }
