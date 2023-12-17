45f5c8f68e4779e4bfd08105944b94ea32b120d9
unknown <uyerandisr@gmail.com>
2023-12-16 23:50:10 -0500

    Resolve the Cors error, i can show the list from the db

--- src/app/dashboard/pages/student-list/student-list.component.ts
+++ src/app/dashboard/pages/student-list/student-list.component.ts
@@ -3,19 +3,20 @@ import { Component, inject, signal } from '@angular/core';
 import { TitleComponent } from '@shared/title/title.component';
 import { StudentListService } from '../../../services/student-list.service';
 import { HttpClient } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-student-list',
   standalone: true,
-  imports: [CommonModule, TitleComponent,],
+  imports: [CommonModule, TitleComponent, RouterModule],
   templateUrl: './student-list.component.html',
   styleUrl: './student-list.component.css'
 })
 export default class StudentListComponent 
 {
   student: any;
-  public studentListService = inject(StudentListService);
-  public showContent = signal(false);
+  public studentListService = inject(StudentListService)
+  public showContent = signal(false)
   
 public constructor()
 {}
@@ -28,16 +29,17 @@ public FillStudentData(): void
           console.log("Hello world");
       },
       error: (error: any) => {
-          console.log(error)
+          // console.log(error)
       },
   });
   console.log(this.student + ' cant');
+  this.showContent.update(value => !value);
 }
 
-public toggleContent() 
-    {
-        this.showContent.update(value => !value);  
-    }
+// public toggleContent() 
+//     {
+//         this.showContent.update(value => !value);  
+//     }
 
 }
 
--- src/app/services/student-list.service.ts
+++ src/app/services/student-list.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
@@ -7,17 +8,22 @@ import { Injectable, inject } from '@angular/core';
 export class StudentListService 
 {
   private http = inject(HttpClient);
+  student: any;
   
   constructor() 
   { 
     console.log('cargando');
     // console.log(this.getAllStudent().forEach.length);
-    
+    this.http.get('http://localhost:8000/student')
+    .subscribe(res =>{
+      this.student = res;
+      console.log('studentList service')
+    })
   }
 
   public getAllStudent()
   {
-    console.log(this.http.get('http://localhost:8000/student'))
+    // console.log(this.http.get('http://localhost:8000/student'))
     return this.http.get('http://localhost:8000/student');
   }
   
