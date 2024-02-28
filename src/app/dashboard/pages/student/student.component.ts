3c8450894f7c25e3749b623d6364dd8c20836bca
unknown <uyerandisr@gmail.com>
2024-02-27 23:37:34 -0500

    implementing toast error

--- src/app/dashboard/pages/student/student.component.ts
+++ src/app/dashboard/pages/student/student.component.ts
@@ -7,30 +7,30 @@ import { ViewChild} from '@angular/core';
 import {MatTable, MatTableModule} from '@angular/material/table';
 import {MatButtonModule} from '@angular/material/button';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+// export interface PeriodicElement {
+//   name: string;
+//   position: number;
+//   weight: number;
+//   symbol: string;
+// }
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+// ];
 @Component({
   selector: 'app-student',
   standalone: true,
   imports: 
   [
-    CommonModule, TitleComponent, RouterModule, 
+    CommonModule, RouterModule, 
     MatCardModule,MatButtonModule, MatTableModule,
   ],
   templateUrl: './student.component.html',
@@ -38,22 +38,22 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export default class StudentComponent 
 {
-  list: [] = [];
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = [...ELEMENT_DATA];
+  // list: [] = [];
+  // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  // dataSource = [...ELEMENT_DATA];
 
-  @ViewChild(MatTable)
-  table!: MatTable<PeriodicElement>;
+  // @ViewChild(MatTable)
+  // table!: MatTable<PeriodicElement>;
 
-  addData() {
-    const randomElementIndex = Math.floor(Math.random() * ELEMENT_DATA.length);
-    this.dataSource.push(ELEMENT_DATA[randomElementIndex]);
-    this.table.renderRows();
-  }
+  // addData() {
+  //   const randomElementIndex = Math.floor(Math.random() * ELEMENT_DATA.length);
+  //   this.dataSource.push(ELEMENT_DATA[randomElementIndex]);
+  //   this.table.renderRows();
+  // }
 
-  removeData() {
-    this.dataSource.pop();
-    this.table.renderRows();
-  }
+  // removeData() {
+  //   this.dataSource.pop();
+  //   this.table.renderRows();
+  // }
    
 }
